60dc8495bf6c7867317330f6b05fbc6737d339e6
r31sr4r <rafael.reis.a@gmail.com>
2022-07-31 12:50:24 +0000

    feat: implementado category.repository

--- src/category/infra/repository/category-in-memory.repository.ts
+++ src/category/infra/repository/category-in-memory.repository.ts
@@ -1,7 +1,21 @@
-import { SearchableInMemoryRepository } from '../../../@seedwork/domain/repository/in-memory.repository';
+import { InMemorySearchableRepository } from '../../../@seedwork/domain/repository/in-memory.repository';
 import { Category } from '../../domain/entities/category';
 import CategoryRepository from '../../domain/repository/category.repository';
 
 export default class CategoryInMemoryRepository
-	extends SearchableInMemoryRepository<Category>
-	implements CategoryRepository {}
+	extends InMemorySearchableRepository<Category>
+	implements CategoryRepository.Repository
+{
+	protected async applyFilter(
+		items: Category[],
+		filter: CategoryRepository.Filter
+	): Promise<Category[]> {
+		if (!filter) {
+			return items;
+		}
+
+		return items.filter((item) => {
+			return item.props.name.toLowerCase().includes(filter.toLowerCase());
+		});
+	}
+}
