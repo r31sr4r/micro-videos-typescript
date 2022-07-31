5f6f291bc05a90e7a826addc7a79b5933acdfdd2
r31sr4r <rafael.reis.a@gmail.com>
2022-07-31 20:36:26 +0000

    feat: list category use case

--- src/category/application/use-cases/get-category.use-case.ts
+++ src/category/application/use-cases/get-category.use-case.ts
@@ -1,19 +1,13 @@
 import UseCase from '../../../@seedwork/application/use-case';
 import CategoryRepository from '../../domain/repository/category.repository';
-import { CategoryOutput } from '../dto/category-output.dto';
+import { CategoryOutput, CategoryOutputMapper } from '../dto/category-output';
 
 export default class GetCategoryUseCase implements UseCase<Input, Output>  {
 	constructor(private categoryRepository: CategoryRepository.Repository) {}
 
 	async execute(input: Input): Promise<Output> {		
 		const entity = await this.categoryRepository.findById(input.id);
-		return {
-			id: entity.id,
-			name: entity.name,
-			description: entity.description,
-			is_active: entity.is_active,
-			created_at: entity.created_at,
-		};
+		return CategoryOutputMapper.toOutput(entity);
 	}
 }
 
