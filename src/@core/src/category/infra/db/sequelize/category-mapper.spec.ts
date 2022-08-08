a28a2d76f6fed2f015eff6506e407a431ff7f81f
r31sr4r <rafael.reis.a@gmail.com>
2022-08-08 22:10:07 +0000

    test: adicionando mais testes

--- src/@core/src/category/infra/db/sequelize/category-mapper.spec.ts
+++ src/@core/src/category/infra/db/sequelize/category-mapper.spec.ts
@@ -1,7 +1,8 @@
-import { LoadEntityError } from '#seedwork/domain';
+import { LoadEntityError, UniqueEntityId } from '#seedwork/domain';
 import { Sequelize } from 'sequelize-typescript';
 import CategoryModelMapper from './category-mapper';
 import { CategoryModel } from './category-model';
+import { Category } from '#category/domain';
 
 describe('CategoryMapper Unit Tests', () => {
 	let sequelize: Sequelize;
@@ -27,19 +28,60 @@ describe('CategoryMapper Unit Tests', () => {
 		const model = CategoryModel.build({
 			id: '02b090cf-5658-4073-b242-9bf64915b3ad',
 		});
-        try {
-            CategoryModelMapper.toEntity(model);
-            fail('The category is valid but an error was expected');            
-        } catch (err) {
-            expect(err).toBeInstanceOf(LoadEntityError);            
-            expect(err.error).toMatchObject({
-                name: [
-                    'name should not be empty',
-                    'name must be a string',
-                    'name must be shorter than or equal to 255 characters',
-                ]
-            });
-            expect(err.message).toBe('An entity could not be loaded');            
-        }
+		try {
+			CategoryModelMapper.toEntity(model);
+			fail('The category is valid but an error was expected');
+		} catch (err) {
+			expect(err).toBeInstanceOf(LoadEntityError);
+			expect(err.error).toMatchObject({
+				name: [
+					'name should not be empty',
+					'name must be a string',
+					'name must be shorter than or equal to 255 characters',
+				],
+			});
+			expect(err.message).toBe('An entity could not be loaded');
+		}
+	});
+
+	it('should throw a generic error', async () => {
+		const error = new Error('An error has occurred');
+		const spyValidate = jest
+			.spyOn(Category, 'validate')
+			.mockImplementation(() => {
+				throw error;
+			});
+		const model = CategoryModel.build({
+			id: '02b090cf-5658-4073-b242-9bf64915b3ad',
+		});
+
+		expect(() => CategoryModelMapper.toEntity(model)).toThrow(error);
+		expect(spyValidate).toHaveBeenCalled();
+		spyValidate.mockRestore();
+	});
+
+	it('should map a category model to a category entity', async () => {
+		const created_at = new Date();
+		const model = CategoryModel.build({
+			id: '02b090cf-5658-4073-b242-9bf64915b3ad',
+			name: 'Category 1',
+			description: 'Description 1',
+			is_active: true,
+			created_at,
+		});
+        
+		const entity = CategoryModelMapper.toEntity(model);
+
+		expect(entity.toJSON()).toStrictEqual(
+			new Category(
+				{
+					name: 'Category 1',
+					description: 'Description 1',
+					is_active: true,
+					created_at,
+				},
+				new UniqueEntityId('02b090cf-5658-4073-b242-9bf64915b3ad')
+			).toJSON()
+		);
 	});
 });
