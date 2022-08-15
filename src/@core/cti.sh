226638d907a8b20e08b1cc951e5c24f34d69c1db
r31sr4r <rafael.reis.a@gmail.com>
2022-08-15 23:33:24 +0000

    build: :building_construction: cti and build changes

--- src/@core/cti.sh
+++ src/@core/cti.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
 npm run cti create './src/@seedwork/application' -- -i '*spec.ts' -b  &&
-npm run cti create './src/@seedwork/domain' -- -i '*spec.ts' -b &&
+npm run cti create './src/@seedwork/domain' -- -i '*spec.ts' -e 'tests' -b &&
 npm run cti create './src/@seedwork/infra' -- -i '*spec.ts' -b &&
 
 npm run cti create './src/category/application' -- -i '*spec.ts' -b &&
 npm run cti create './src/category/domain' -- -i '*spec.ts' -b &&
 npm run cti create './src/category/infra' -- -i '*spec.ts' -b
+
