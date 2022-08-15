604168dacdf8e54d11b26ee787c8020727a74d82
r31sr4r <rafael.reis.a@gmail.com>
2022-08-15 22:17:51 +0000

    refactor: schema

--- src/nestjs/src/app.controller.spec.ts
+++ src/nestjs/src/app.controller.spec.ts
@@ -2,7 +2,7 @@ import { ConfigService } from '@nestjs/config';
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from './config/config.module';
+import { ConfigModule, CONFIG_SCHEMA_TYPE } from './config/config.module';
 
 describe('AppController', () => {
     let appController: AppController;
@@ -15,7 +15,12 @@ describe('AppController', () => {
         }).compile();
 
         appController = app.get<AppController>(AppController);
-        console.log(app.get(ConfigService).get('DB_VENDOR'));
+
+        const configService: ConfigService = app.get<ConfigService<CONFIG_SCHEMA_TYPE>>(ConfigService);
+
+        const db_vendor = configService.get<CONFIG_SCHEMA_TYPE['DB_VENDOR']>('DB_VENDOR');
+
+        console.log(db_vendor);
     });
 
     describe('root', () => {
