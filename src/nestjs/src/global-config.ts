bb92a99a27f836817daf7a6f1a8fda92a49e0f86
r31sr4r <rafael.reis.a@gmail.com>
2022-10-11 19:37:50 -0300

    feat: add entity validation filters

--- src/nestjs/src/global-config.ts
+++ src/nestjs/src/global-config.ts
@@ -5,6 +5,7 @@ import {
 } from '@nestjs/common';
 import { WrapperDataInterceptor } from './@share/interceptors/wrapper-data.interceptor';
 import { Reflector } from '@nestjs/core';
+import { EntityValidationErrorFilter } from './@share/exception-filters/entity-validation-error.filter';
 
 export function applyGlobalConfig(app: INestApplication) {
     app.useGlobalPipes(new ValidationPipe({
@@ -14,4 +15,7 @@ export function applyGlobalConfig(app: INestApplication) {
         new WrapperDataInterceptor(),
         new ClassSerializerInterceptor(app.get(Reflector)),
     );
+    app.useGlobalFilters(
+        new EntityValidationErrorFilter(),
+    )
 }
